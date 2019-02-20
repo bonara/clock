73b99d40b6fbea46a91e1e306fdc99d9b6d15951
Nara <naraa_520@yahoo.com>
2019-02-20 10:47:49 -0800

    Created a clock method and added tests

--- clock_spec.rb
+++ clock_spec.rb
@@ -1,1 +1,47 @@
 # clock_spec.rb
+require 'minitest/autorun'
+require 'minitest/reporters'
+require_relative 'clock'
+
+Minitest::Reporters.use!
+
+# Describe blocks group tests
+describe "clock" do 
+  # It blocks are tests
+  it "returns a string" do
+    #Arrange
+    hours = 8
+    minutes = 14
+    seconds = 3
+
+    #Act
+    result = clock(hours, minutes, seconds)
+
+    #Assert
+    expect(result).must_be_instance_of String
+  end
+  it "pads number with leading zeros" do
+    #Arrange
+    hours = 8
+    minutes = 4
+    seconds = 3
+
+    #Act
+    result = clock(hours, minutes, seconds)
+
+    #Assert
+    expect(result).must_equal "08:04:03"
+  end
+  it "handles 2-digit numbers" do
+    #Arrange
+    hours = 10
+    minutes = 22
+    seconds = 35
+
+    #Act
+    result = clock(hours, minutes, seconds)
+
+    #Assert
+    expect(result).must_equal "10:22:35"
+  end
+end
